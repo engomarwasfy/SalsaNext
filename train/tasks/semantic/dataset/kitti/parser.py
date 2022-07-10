e2cc079d815a298023b0af9a8b3d7642cb6555d8
Sourcery AI <>
2022-07-10 16:36:01 +0000

    'Refactored by Sourcery'

--- train/tasks/semantic/dataset/kitti/parser.py
+++ train/tasks/semantic/dataset/kitti/parser.py
@@ -71,209 +71,204 @@ class SemanticKitti(Dataset):
                max_points=150000,   # max number of points present in dataset
                gt=True,
                transform=False):            # send ground truth?
-    # save deats
-    self.root = os.path.join(root, "sequences")
-    self.sequences = sequences
-    self.labels = labels
-    self.color_map = color_map
-    self.learning_map = learning_map
-    self.learning_map_inv = learning_map_inv
-    self.sensor = sensor
-    self.sensor_img_H = sensor["img_prop"]["height"]
-    self.sensor_img_W = sensor["img_prop"]["width"]
-    self.sensor_img_means = torch.tensor(sensor["img_means"],
-                                         dtype=torch.float)
-    self.sensor_img_stds = torch.tensor(sensor["img_stds"],
-                                        dtype=torch.float)
-    self.sensor_fov_up = sensor["fov_up"]
-    self.sensor_fov_down = sensor["fov_down"]
-    self.max_points = max_points
-    self.gt = gt
-    self.transform = transform
+      # save deats
+      self.root = os.path.join(root, "sequences")
+      self.sequences = sequences
+      self.labels = labels
+      self.color_map = color_map
+      self.learning_map = learning_map
+      self.learning_map_inv = learning_map_inv
+      self.sensor = sensor
+      self.sensor_img_H = sensor["img_prop"]["height"]
+      self.sensor_img_W = sensor["img_prop"]["width"]
+      self.sensor_img_means = torch.tensor(sensor["img_means"],
+                                           dtype=torch.float)
+      self.sensor_img_stds = torch.tensor(sensor["img_stds"],
+                                          dtype=torch.float)
+      self.sensor_fov_up = sensor["fov_up"]
+      self.sensor_fov_down = sensor["fov_down"]
+      self.max_points = max_points
+      self.gt = gt
+      self.transform = transform
 
-    # get number of classes (can't be len(self.learning_map) because there
-    # are multiple repeated entries, so the number that matters is how many
-    # there are for the xentropy)
-    self.nclasses = len(self.learning_map_inv)
+      # get number of classes (can't be len(self.learning_map) because there
+      # are multiple repeated entries, so the number that matters is how many
+      # there are for the xentropy)
+      self.nclasses = len(self.learning_map_inv)
 
-    # sanity checks
+        # sanity checks
 
-    # make sure directory exists
-    if os.path.isdir(self.root):
-      print("Sequences folder exists! Using sequences from %s" % self.root)
-    else:
-      raise ValueError("Sequences folder doesn't exist! Exiting...")
+        # make sure directory exists
+      if os.path.isdir(self.root):
+          print(f"Sequences folder exists! Using sequences from {self.root}")
+      else:
+          raise ValueError("Sequences folder doesn't exist! Exiting...")
 
-    # make sure labels is a dict
-    assert(isinstance(self.labels, dict))
+      # make sure labels is a dict
+      assert(isinstance(self.labels, dict))
 
-    # make sure color_map is a dict
-    assert(isinstance(self.color_map, dict))
+      # make sure color_map is a dict
+      assert(isinstance(self.color_map, dict))
 
-    # make sure learning_map is a dict
-    assert(isinstance(self.learning_map, dict))
+      # make sure learning_map is a dict
+      assert(isinstance(self.learning_map, dict))
 
-    # make sure sequences is a list
-    assert(isinstance(self.sequences, list))
+      # make sure sequences is a list
+      assert(isinstance(self.sequences, list))
 
-    # placeholder for filenames
-    self.scan_files = []
-    self.label_files = []
+      # placeholder for filenames
+      self.scan_files = []
+      self.label_files = []
 
-    # fill in with names, checking that all sequences are complete
-    for seq in self.sequences:
-      # to string
-      seq = '{0:02d}'.format(int(seq))
+        # fill in with names, checking that all sequences are complete
+      for seq in self.sequences:
+          # to string
+          seq = '{0:02d}'.format(int(seq))
 
-      print("parsing seq {}".format(seq))
+          print(f"parsing seq {seq}")
 
-      # get paths for each
-      scan_path = os.path.join(self.root, seq, "velodyne")
-      label_path = os.path.join(self.root, seq, "labels")
+          # get paths for each
+          scan_path = os.path.join(self.root, seq, "velodyne")
+          label_path = os.path.join(self.root, seq, "labels")
 
-      # get files
-      scan_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
-          os.path.expanduser(scan_path)) for f in fn if is_scan(f)]
-      label_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
-          os.path.expanduser(label_path)) for f in fn if is_label(f)]
+          # get files
+          scan_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
+              os.path.expanduser(scan_path)) for f in fn if is_scan(f)]
+          label_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
+              os.path.expanduser(label_path)) for f in fn if is_label(f)]
 
-      # check all scans have labels
-      if self.gt:
-        assert(len(scan_files) == len(label_files))
+          # check all scans have labels
+          if self.gt:
+            assert(len(scan_files) == len(label_files))
 
-      # extend list
-      self.scan_files.extend(scan_files)
-      self.label_files.extend(label_files)
+          # extend list
+          self.scan_files.extend(scan_files)
+          self.label_files.extend(label_files)
 
-    # sort for correspondance
-    self.scan_files.sort()
-    self.label_files.sort()
+      # sort for correspondance
+      self.scan_files.sort()
+      self.label_files.sort()
 
-    print("Using {} scans from sequences {}".format(len(self.scan_files),
-                                                    self.sequences))
+      print(f"Using {len(self.scan_files)} scans from sequences {self.sequences}")
 
   def __getitem__(self, index):
-    # get item in tensor shape
-    scan_file = self.scan_files[index]
-    if self.gt:
-      label_file = self.label_files[index]
+      # get item in tensor shape
+      scan_file = self.scan_files[index]
+      if self.gt:
+        label_file = self.label_files[index]
 
-    # open a semantic laserscan
-    DA = False
-    flip_sign = False
-    rot = False
-    drop_points = False
-    if self.transform:
-        if random.random() > 0.5:
-            if random.random() > 0.5:
-                DA = True
-            if random.random() > 0.5:
-                flip_sign = True
-            if random.random() > 0.5:
-                rot = True
-            drop_points = random.uniform(0, 0.5)
+      # open a semantic laserscan
+      DA = False
+      flip_sign = False
+      rot = False
+      drop_points = False
+      if self.transform and random.random() > 0.5:
+          if random.random() > 0.5:
+              DA = True
+          if random.random() > 0.5:
+              flip_sign = True
+          if random.random() > 0.5:
+              rot = True
+          drop_points = random.uniform(0, 0.5)
 
-    if self.gt:
-      scan = SemLaserScan(self.color_map,
-                          project=True,
-                          H=self.sensor_img_H,
-                          W=self.sensor_img_W,
-                          fov_up=self.sensor_fov_up,
-                          fov_down=self.sensor_fov_down,
-                          DA=DA,
-                          flip_sign=flip_sign,
-                          drop_points=drop_points)
-    else:
-      scan = LaserScan(project=True,
-                       H=self.sensor_img_H,
-                       W=self.sensor_img_W,
-                       fov_up=self.sensor_fov_up,
-                       fov_down=self.sensor_fov_down,
-                       DA=DA,
-                       rot=rot,
-                       flip_sign=flip_sign,
-                       drop_points=drop_points)
+      if self.gt:
+        scan = SemLaserScan(self.color_map,
+                            project=True,
+                            H=self.sensor_img_H,
+                            W=self.sensor_img_W,
+                            fov_up=self.sensor_fov_up,
+                            fov_down=self.sensor_fov_down,
+                            DA=DA,
+                            flip_sign=flip_sign,
+                            drop_points=drop_points)
+      else:
+        scan = LaserScan(project=True,
+                         H=self.sensor_img_H,
+                         W=self.sensor_img_W,
+                         fov_up=self.sensor_fov_up,
+                         fov_down=self.sensor_fov_down,
+                         DA=DA,
+                         rot=rot,
+                         flip_sign=flip_sign,
+                         drop_points=drop_points)
 
-    # open and obtain scan
-    scan.open_scan(scan_file)
-    if self.gt:
-      scan.open_label(label_file)
-      # map unused classes to used classes (also for projection)
-      scan.sem_label = self.map(scan.sem_label, self.learning_map)
-      scan.proj_sem_label = self.map(scan.proj_sem_label, self.learning_map)
+      # open and obtain scan
+      scan.open_scan(scan_file)
+      if self.gt:
+        scan.open_label(label_file)
+        # map unused classes to used classes (also for projection)
+        scan.sem_label = self.map(scan.sem_label, self.learning_map)
+        scan.proj_sem_label = self.map(scan.proj_sem_label, self.learning_map)
 
-    # make a tensor of the uncompressed data (with the max num points)
-    unproj_n_points = scan.points.shape[0]
-    unproj_xyz = torch.full((self.max_points, 3), -1.0, dtype=torch.float)
-    unproj_xyz[:unproj_n_points] = torch.from_numpy(scan.points)
-    unproj_range = torch.full([self.max_points], -1.0, dtype=torch.float)
-    unproj_range[:unproj_n_points] = torch.from_numpy(scan.unproj_range)
-    unproj_remissions = torch.full([self.max_points], -1.0, dtype=torch.float)
-    unproj_remissions[:unproj_n_points] = torch.from_numpy(scan.remissions)
-    if self.gt:
-      unproj_labels = torch.full([self.max_points], -1.0, dtype=torch.int32)
-      unproj_labels[:unproj_n_points] = torch.from_numpy(scan.sem_label)
-    else:
-      unproj_labels = []
+      # make a tensor of the uncompressed data (with the max num points)
+      unproj_n_points = scan.points.shape[0]
+      unproj_xyz = torch.full((self.max_points, 3), -1.0, dtype=torch.float)
+      unproj_xyz[:unproj_n_points] = torch.from_numpy(scan.points)
+      unproj_range = torch.full([self.max_points], -1.0, dtype=torch.float)
+      unproj_range[:unproj_n_points] = torch.from_numpy(scan.unproj_range)
+      unproj_remissions = torch.full([self.max_points], -1.0, dtype=torch.float)
+      unproj_remissions[:unproj_n_points] = torch.from_numpy(scan.remissions)
+      if self.gt:
+        unproj_labels = torch.full([self.max_points], -1.0, dtype=torch.int32)
+        unproj_labels[:unproj_n_points] = torch.from_numpy(scan.sem_label)
+      else:
+        unproj_labels = []
 
-    # get points and labels
-    proj_range = torch.from_numpy(scan.proj_range).clone()
-    proj_xyz = torch.from_numpy(scan.proj_xyz).clone()
-    proj_remission = torch.from_numpy(scan.proj_remission).clone()
-    proj_mask = torch.from_numpy(scan.proj_mask)
-    if self.gt:
-      proj_labels = torch.from_numpy(scan.proj_sem_label).clone()
-      proj_labels = proj_labels * proj_mask
-    else:
-      proj_labels = []
-    proj_x = torch.full([self.max_points], -1, dtype=torch.long)
-    proj_x[:unproj_n_points] = torch.from_numpy(scan.proj_x)
-    proj_y = torch.full([self.max_points], -1, dtype=torch.long)
-    proj_y[:unproj_n_points] = torch.from_numpy(scan.proj_y)
-    proj = torch.cat([proj_range.unsqueeze(0).clone(),
-                      proj_xyz.clone().permute(2, 0, 1),
-                      proj_remission.unsqueeze(0).clone()])
-    proj = (proj - self.sensor_img_means[:, None, None]
-            ) / self.sensor_img_stds[:, None, None]
-    proj = proj * proj_mask.float()
+      # get points and labels
+      proj_range = torch.from_numpy(scan.proj_range).clone()
+      proj_xyz = torch.from_numpy(scan.proj_xyz).clone()
+      proj_remission = torch.from_numpy(scan.proj_remission).clone()
+      proj_mask = torch.from_numpy(scan.proj_mask)
+      if self.gt:
+        proj_labels = torch.from_numpy(scan.proj_sem_label).clone()
+        proj_labels = proj_labels * proj_mask
+      else:
+        proj_labels = []
+      proj_x = torch.full([self.max_points], -1, dtype=torch.long)
+      proj_x[:unproj_n_points] = torch.from_numpy(scan.proj_x)
+      proj_y = torch.full([self.max_points], -1, dtype=torch.long)
+      proj_y[:unproj_n_points] = torch.from_numpy(scan.proj_y)
+      proj = torch.cat([proj_range.unsqueeze(0).clone(),
+                        proj_xyz.clone().permute(2, 0, 1),
+                        proj_remission.unsqueeze(0).clone()])
+      proj = (proj - self.sensor_img_means[:, None, None]
+              ) / self.sensor_img_stds[:, None, None]
+      proj = proj * proj_mask.float()
 
-    # get name and sequence
-    path_norm = os.path.normpath(scan_file)
-    path_split = path_norm.split(os.sep)
-    path_seq = path_split[-3]
-    path_name = path_split[-1].replace(".bin", ".label")
+      # get name and sequence
+      path_norm = os.path.normpath(scan_file)
+      path_split = path_norm.split(os.sep)
+      path_seq = path_split[-3]
+      path_name = path_split[-1].replace(".bin", ".label")
 
-    # return
-    return proj, proj_mask, proj_labels, unproj_labels, path_seq, path_name, proj_x, proj_y, proj_range, unproj_range, proj_xyz, unproj_xyz, proj_remission, unproj_remissions, unproj_n_points
+      # return
+      return proj, proj_mask, proj_labels, unproj_labels, path_seq, path_name, proj_x, proj_y, proj_range, unproj_range, proj_xyz, unproj_xyz, proj_remission, unproj_remissions, unproj_n_points
 
   def __len__(self):
     return len(self.scan_files)
 
   @staticmethod
   def map(label, mapdict):
-    # put label from original values to xentropy
-    # or vice-versa, depending on dictionary values
-    # make learning map a lookup table
-    maxkey = 0
-    for key, data in mapdict.items():
-      if isinstance(data, list):
-        nel = len(data)
+      # put label from original values to xentropy
+      # or vice-versa, depending on dictionary values
+      # make learning map a lookup table
+      maxkey = 0
+      for key, data in mapdict.items():
+          nel = len(data) if isinstance(data, list) else 1
+          if key > maxkey:
+            maxkey = key
+      # +100 hack making lut bigger just in case there are unknown labels
+      if nel > 1:
+        lut = np.zeros((maxkey + 100, nel), dtype=np.int32)
       else:
-        nel = 1
-      if key > maxkey:
-        maxkey = key
-    # +100 hack making lut bigger just in case there are unknown labels
-    if nel > 1:
-      lut = np.zeros((maxkey + 100, nel), dtype=np.int32)
-    else:
-      lut = np.zeros((maxkey + 100), dtype=np.int32)
-    for key, data in mapdict.items():
-      try:
-        lut[key] = data
-      except IndexError:
-        print("Wrong key ", key)
-    # do the mapping
-    return lut[label]
+        lut = np.zeros((maxkey + 100), dtype=np.int32)
+      for key, data in mapdict.items():
+        try:
+          lut[key] = data
+        except IndexError:
+          print("Wrong key ", key)
+      # do the mapping
+      return lut[label]
 
 
 class Parser():
@@ -372,22 +367,19 @@ class Parser():
       self.testiter = iter(self.testloader)
 
   def get_train_batch(self):
-    scans = self.trainiter.next()
-    return scans
+      return self.trainiter.next()
 
   def get_train_set(self):
     return self.trainloader
 
   def get_valid_batch(self):
-    scans = self.validiter.next()
-    return scans
+      return self.validiter.next()
 
   def get_valid_set(self):
     return self.validloader
 
   def get_test_batch(self):
-    scans = self.testiter.next()
-    return scans
+      return self.testiter.next()
 
   def get_test_set(self):
     return self.testloader
--- train/tasks/semantic/modules/Lovasz_Softmax.py
+++ train/tasks/semantic/modules/Lovasz_Softmax.py
@@ -64,7 +64,7 @@ def lovasz_grad(gt_sorted):
     union = gts + (1 - gt_sorted).float().cumsum(0)
     jaccard = 1. - intersection / union
     if p > 1:  # cover 1-pixel case
-        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+        jaccard[1:p] = jaccard[1:p] - jaccard[:-1]
     return jaccard
 
 
@@ -78,12 +78,19 @@ def lovasz_softmax(probas, labels, classes='present', per_image=False, ignore=No
       per_image: compute the loss per image instead of per batch
       ignore: void class labels
     """
-    if per_image:
-        loss = mean(lovasz_softmax_flat(*flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), classes=classes)
-                    for prob, lab in zip(probas, labels))
-    else:
-        loss = lovasz_softmax_flat(*flatten_probas(probas, labels, ignore), classes=classes)
-    return loss
+    return (
+        mean(
+            lovasz_softmax_flat(
+                *flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore),
+                classes=classes
+            )
+            for prob, lab in zip(probas, labels)
+        )
+        if per_image
+        else lovasz_softmax_flat(
+            *flatten_probas(probas, labels, ignore), classes=classes
+        )
+    )
 
 
 def lovasz_softmax_flat(probas, labels, classes='present'):
